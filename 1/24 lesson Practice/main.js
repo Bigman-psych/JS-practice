34b5e891b9a354a3b546ed81e47c9b4a02c34fd2
Bigman-psych <sergeykyzmenko21@gmail.com>
2022-12-13 00:25:19 +0200

    lesson 24-25

---
+++ 1/24 lesson Practice/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// let x = 5; alert( x++ ); 
+
+// [ ] + false - null + true 
+
+// let y = 1; let x = y = 2; alert(x); 
+
+// [ ] + 1 + 2
+
+// alert( "1"[0] )
+
+// 2 && 1 && null && 0 && undefined 
+
+// !!( a && b ) и (a && b)
+
+// alert( null || 2 && 3 || 4 ); 
+
+// a = [1, 2, 3]; b = [1, 2, 3]; Правда ли что a == b 
+
+// alert( +"Infinity" ); 
+
+// "Ёжик" > "яблоко"
+
+0 || "" || 2 || undefined || true || falsе 
+
